674c8841d38c89c71192ce413abed42d63681b77
iofq <cjriddz@protonmail.com>
2022-07-05 13:46:28 -0500

    added current listener count

--- main.go
+++ main.go
@@ -5,23 +5,40 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+    "strconv"
+    "strings"
 )
 
-var PORT = ":8000"
+var PORT = ":8080"
+
+func connListeners(w http.ResponseWriter, r *http.Request) {
+    cmd := "/usr/bin/ss | grep 8000 | wc -l"
+	out := exec.Command("bash", "-c", cmd)
+    stdout, err := out.Output()
+    if err != nil  {
+        stdout = []byte("Current track not found")
+    }
+    x := strings.ReplaceAll(string(stdout), "\n", "")
+    conn, err := strconv.Atoi(x)
+    if err != nil {
+        fmt.Println(err)
+    }
+    conn = conn / 3
+    fmt.Fprintf(w, "Connected listeners: %d", conn)
+}
 
 func currentTrack(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	cmd := exec.Command("mpc status | head -n 2")
-	err := cmd.Run()
-	stdout := []byte("Current track not found")
-	if err == nil {
-		stdout, err = cmd.Output()
-	}
-	fmt.Fprintf(w, string(stdout))
+    cmd := "/usr/bin/mpc status | head -n 1"
+	out := exec.Command("bash", "-c", cmd)
+    stdout, err := out.Output()
+    if err != nil  {
+        stdout = []byte("Current track not found")
+    }
+	fmt.Fprintf(w, strings.ReplaceAll(string(stdout), "\n", "</br>"))
 }
 
 func skipTrack(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.Command("mpc next")
+	cmd := exec.Command("/usr/bin/mpc", "next")
 	err := cmd.Run()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -31,6 +48,7 @@ func skipTrack(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/listeners", connListeners)
 	http.HandleFunc("/current", currentTrack)
 	http.HandleFunc("/skip", skipTrack)
 	log.Fatal(http.ListenAndServe(PORT, nil))
--- static/index.js
+++ static/index.js
@@ -1,10 +1,33 @@
 const title = document.getElementById("track")
-const track_endpoint = "http://radio.10110110.xyz/current"
+const track_endpoint = "https://radio.10110110.xyz/current"
+
+const count = document.getElementById("count")
+const count_endpoint = "https://radio.10110110.xyz/listeners"
+
+const audio = document.getElementById("audio")
+const skip_endpoint = "https://radio.10110110.xyz/skip"
+
+async function connListeners() {
+    const resp = await fetch(count_endpoint)
+    const body = await resp.text()
+    count.innerHTML = body
+}
 
 async function currentTrack() {
     const resp = await fetch(track_endpoint)
     const body = await resp.text()
     title.innerHTML = body
 }
+
+async function skipTrack() {
+    await fetch(skip_endpoint)
+    currentTrack()
+    audio.pause();
+    audio.play();
+}
+
+audio.volume = 0.5;
 currentTrack()
-window.setTimeout(currentTrack, 60 * 1000)
+connListeners()
+window.setInterval(currentTrack, 5 * 1000)
+window.setInterval(connListeners, 5 * 1000)
